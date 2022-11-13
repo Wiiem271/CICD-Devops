e9c5c6ff30a2bca572f1b11213a6dd829bdd99d4
mokht825 <57508853+mokht825@users.noreply.github.com>
2022-11-13 22:34:21 +0100

    Update FournisseurServiceUnitTest.java

--- src/test/java/com/esprit/examen/services/FournisseurServiceUnitTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceUnitTest.java
@@ -1,4 +1,4 @@
- package com.esprit.examen.services;
+/* package com.esprit.examen.services;
 
 //import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -78,4 +78,6 @@ import com.esprit.examen.repositories.FournisseurRepository;
     
 
 }
+
+*/
 
