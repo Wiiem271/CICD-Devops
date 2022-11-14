79a3b845a9858162e7faa1be40254192dab1b003
chaimaaskri <chaima.askri@esprit.tn>
2022-11-14 21:45:31 +0100

    update2

--- src/test/java/com/esprit/examen/services/PoduitMockitoTest.java
+++ src/test/java/com/esprit/examen/services/PoduitMockitoTest.java
@@ -12,12 +12,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.esprit.examen.entities.Produit;
@@ -25,7 +27,7 @@ import com.esprit.examen.repositories.ProduitRepository;
 import com.esprit.examen.services.ProduitServiceImpl;
 
 @SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 class PoduitMockitoTest{
 
 @Mock
