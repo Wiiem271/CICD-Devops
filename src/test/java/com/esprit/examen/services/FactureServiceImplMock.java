55f4ae1905955c5656107226adf5ec3b54c013f8
bilelfarah <72948636+bilelfarah@users.noreply.github.com>
2022-11-17 11:56:43 +0100

    Update FactureServiceImplMock.java

--- src/test/java/com/esprit/examen/services/FactureServiceImplMock.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplMock.java
@@ -1,4 +1,4 @@
-package com.esprit.examen.services;
+/*package com.esprit.examen.services;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
@@ -55,6 +55,6 @@ public class FactureServiceImplMock {
 		}
 		
 		
-	}
+	}*/
 
 
